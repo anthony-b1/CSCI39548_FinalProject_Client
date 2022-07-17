b529b0e9351905fd6d0e95023cef6d61299ccb56
Anthony <44449574+anthony-b1@users.noreply.github.com>
2022-07-16 23:14:09 -0400

    Added placeholder to input

--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -52,22 +52,22 @@ const EditCampusView = (props) => {
           </div>
           <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Name: </label>
-            <input type="text" name="name" onChange ={(e) => handleChange(e)} required/>
+            <input type="text" name="name" placeholder = {campus.name} onChange ={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Image URL: </label>
-            <input type="text" name="imageUrl" placeholder = "Optional" onChange={(e) => handleChange(e)} />
+            <input type="text" name="imageUrl" placeholder = {campus.imageUrl} onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
-            <input type="text" name="address" onChange={(e) => handleChange(e)} required/>
+            <input type="text" name="address" placeholder = {campus.address} onChange={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
-            <textarea type="text" name="description" placeholder = "Optional" onChange={(e) => handleChange(e)} />
+            <textarea type="text" name="description" placeholder = {campus.description} onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
 
